5abefe40250af5821c1c6c4f078676b3b40befec
harkiransinghmahun <68849256+harkiransinghmahun@users.noreply.github.com>
2021-07-30 09:30:52 +0100

    How to solve problems cheat sheet

---
+++ Solving Coding Problems/SolveCodingProblems.js
@@ -0,0 +1,24 @@
+// What are companies looking for?
+// 1. Analytic Skills
+// 2. Coding Skills
+// 3. Technical Skills
+// 4. Communication Skills
+
+
+// Data Strucutres: 
+// 1. Array
+// 2. Trees
+// 3. Stacks
+// 4. Tries
+// 5. Queues
+// 6. Graphs
+// 7. Linked Lists
+// 8. Hash Tables
+
+
+// Algorithms:
+// 1. Sorting
+// 2. Dynamic Programming
+// 3. BFS + DFS (Searching)
+// 4. Recursion
+
